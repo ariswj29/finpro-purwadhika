cc270ce7edca6916a0ddc55b88d01f18ad4e6dde
ariswj29 <ariswj29@gmail.com>
2024-08-23 16:06:57 +0700

    build: starter kit

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -12,3 +12,7 @@ config({ path: resolve(__dirname, `../${envFile}.local`), override: true });
 
 export const PORT = process.env.PORT || 8000;
 export const DATABASE_URL = process.env.DATABASE_URL || '';
+export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
+export const SALT = parseInt(process.env.SALT || '10');
+export const RAJAONGKIR_API_KEY = process.env.RAJAONGKIR_API_KEY || 'my-secret';
+export const RAJAONGKIR_API_URL = process.env.RAJAONGKIR_API_URL || 'my-secret';
